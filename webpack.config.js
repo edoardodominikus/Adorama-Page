bb34e38583538d4d2d4c8cb77bc44be4ea0c078c
edoardodominikus <s108006222@m108.nthu.edu.tw>
2021-07-22 15:51:05 +0800

    setup webpack and browser sync

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    watch: true,
+    plugins: [
+      new BrowserSyncPlugin({
+        // browse to http://localhost:3000/ during development,
+        // ./public directory is being served
+        host: 'localhost',
+        port: 8000,
+        server: { baseDir: ['dist'] }
+      })
+    ]
+  }
